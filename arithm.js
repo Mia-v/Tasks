8d71cd618ad780125f7f9296337423b515e6e06f
Mia-v <vasilyeva.mariya@gmail.com>
2020-08-04 10:53:46 +0200

    Created modules with simple arithmetic functions

---
+++ arithm.js
@@ -0,0 +1,9 @@
+const arithm = {
+    getSum: function(num1, num2 ){ return num1 + num2;},
+    getDiff: function(num1, num2) { return num1 - num2;},
+    getProduct: function(num1, num2) { return num1 * num2;},
+    getQuotient: function(num1, num2) { return num1 / num2;},
+}
+
+
+module.exports = arithm;
---
+++ index.js
@@ -0,0 +1,3 @@
+const arithm = require('./arithm');
+
+console.log(arithm.getDiff(arithm.getProduct(2, 3), arithm.getQuotient(9, 3)));
