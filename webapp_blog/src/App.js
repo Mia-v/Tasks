2191b4da92408355986e9d65226509c4142a5f7d
Mia-v <vasilyeva.mariya@gmail.com>
2020-08-16 18:02:55 +0200

    Added Redux

--- webapp_blog/src/App.js
+++ webapp_blog/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.scss';
+import { Provider } from 'react-redux';
+import store from './Redux/store.js';
+
 import Navigation from "./components/Navigation/Navigation.js";
 import Header from './components/Header/Header.js';
 import InpageNav from './components/InpageNav/InpageNav.js';
@@ -7,22 +10,25 @@ import Content from './components/Content/Content.js';
 import Additions from './components/Additions/Additions.js';
 import Footer from './components/Footer/Footer.js';
 
+
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <div className="container-main">
-        <Header />
-        <div className="content-n-footer">
-          <div className="wrapper">
-            <InpageNav />
-            <Content />
-            <Additions />
+    <Provider store={store}>
+      <div className="App">
+        <Navigation />
+        <div className="container-main">
+          <Header />
+          <div className="content-n-footer">
+            <div className="wrapper">
+              <InpageNav />
+              <Content />
+              <Additions />
+            </div>
+            <Footer />
           </div>
-          <Footer />
         </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
